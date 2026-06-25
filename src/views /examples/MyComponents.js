b8587774b83bc05b6b1db2da6101dcd9315c9f8a
Phan Thanh Thien Phu <thienphu579@gmail.com>
2026-06-25 09:57:16 +0700

    event onChange

--- src/views /examples/MyComponents.js
+++ src/views /examples/MyComponents.js
@@ -1,12 +1,36 @@
 import React from "react";
 
 class MyComponents extends React.Component {
+
+
+    state = { 
+        name: "Thien Phu Nguyen", 
+        age: 20
+    }
+    handleButton = () => {
+        alert("click me"); 
+    }
+    handleOnchange = (e) => {
+        this.setState({ name: e.target.value });
+    }
   render() {
+    console.log(">>> call render: ", this.state);
     return (
-      <div>
+        
+<   >
+      <div className="first"> 
         <h1>My Components</h1>
-        <p>This is a simple React component.</p>
       </div>
+      <div className="second">
+        My Name is  <input onChange= {(e) => this.handleOnchange(e)}   /> <h1> {this.state.name}</h1>
+
+      </div>
+     <div className="third"> 
+        <button onClick={()=> this.handleButton()}> Click Me</button>
+     </div>
+       
+      
+    </>
     );
   } 
 }
